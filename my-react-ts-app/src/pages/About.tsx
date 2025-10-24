3f5911c404c7b86b71c9c1f35221d0a1bea3e167
Chanwit <66160224@go.buu.ac.th>
2025-10-24 20:13:21 +0700

    add shadcn

--- my-react-ts-app/src/pages/About.tsx
+++ my-react-ts-app/src/pages/About.tsx
@@ -1,6 +1,13 @@
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input"
 function About() {
   return (
-    <h2>about</h2>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <Input type="email" placeholder="Email" />
+    </div>
+
+    
   );
 }
 
--- my-react-ts-app/src/pages/App.tsx
+++ my-react-ts-app/src/pages/App.tsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../styles/css/App.css";
 import Navbar from "../components/Navbar";
 import About from "./About";
+import Statistics from "./Statistics";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-       
         <Route element={<Navbar />}>
           <Route path="/" element={<About />} />
-          
-          </Route>
+          <Route path="/bmi/statistics" element={<Statistics />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
