4ceeb6c207db26830fb7966a131422a635e9b940
tonkungth11 <nnvvvton@gmail.com>
2025-10-24 22:55:32 +0700

    feat(view)navbar updatw

--- my-react-ts-app/src/pages/About.tsx
+++ my-react-ts-app/src/pages/About.tsx
@@ -2,9 +2,8 @@ import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input"
 function About() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-      <Input type="email" placeholder="Email" />
+    <div className="">
+    
     </div>
 
     
