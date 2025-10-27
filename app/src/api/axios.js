457da565e94462e8998a41aaa604cd5faef8e9a5
Pakkapon <Pakkapon2547@gmail.com>
2025-10-28 00:33:41 +0700

    fix wtf my friend wrote

--- app/src/api/axios.js
+++ app/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_BASE_URL || "/api";
 
 const api = axios.create({
-  baseURL: API_URL, // backend port
+  baseURL: `${API_URL}/api`, // backend port
   //withCredentials: true, // ถ้าใช้ cookie auth
   headers: { "Content-Type": "application/json" },
 });
--- app/src/services/history.ts
+++ app/src/services/history.ts
@@ -1,7 +1,7 @@
 import api from "@/api/axios";
 
 export async function fetchBmiHistory(usEmail: string): Promise<any> {
-  const res = await api.get("http://localhost:4004/api/bmi/history", {
+  const res = await api.get("/bmi/history", {
     params: { email: usEmail }, // ✅ ส่ง email เป็น query parameter
     headers: { "Content-Type": "application/json" },
   });
